7ce162b9b2e288a864e1eee0b8952ad2280088bb
Jose Felix <felizperez1097@gmail.com>
2022-05-30 19:05:43 -0500

    Laboratorio Solucionado

--- calculator.js
+++ calculator.js
@@ -1,14 +1,32 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0
+  } else if(a && !b){
+    return 0 + a
+  } else if(!a && b){
+    return 0 + b
+  }
+  return a + b
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b){
+    return 0
+  }else if(a && !b){ 
+    return a - 0
+  }
+  return a - b
 }
 
 function divide(a, b) {
-  return;
+  if (a && b){
+    return a / b  
+  }else {
+    return error('No puedes dividir entre 0');
+  }
 }
+
+
 
 function multiply(a, b) {
   return a * b;
@@ -16,3 +34,9 @@ function multiply(a, b) {
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
+/*
+function subtract(a,b){
+  return a && b ? a - b : a ? a : b ? b : 0
+}
+
+*/
